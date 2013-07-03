0aab1f5c75f019fcd9aa4a714814adc7a79a83b1
Vishnu <vishnu.garg@shephertz.co.in>
2013-07-03 21:51:08 +0530

    Unity3D Push notification sample
    Signed-off-by: Vishnu <vishnu.garg@shephertz.co.in>

---
+++ Assets/Constants.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+
+public class Constants {
+
+	public string apiKey ="<YOUR API KEY>";
+	public string secretKey="<YOUR SECRET KEY>";
+	public string projectNo="<YOUR PROJECT NO>";
+	public string userId="<YOUR USER ID>";
+    public string callBackMethod="<CALLBACK METHOD>";
+	//public string callBackMethod="Success";
+	public string gameObjectName="<GAME OBJECT NAME>";
+	//public string gameObjectName="app42Msg";
+}
---
+++ Assets/PushSample.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class PushSample: MonoBehaviour
+{
+	Constants constants = new Constants ();
+#if UNITY_ANDROID
+	void OnGUI()
+    {
+        if (GUI.Button(new Rect(50, 100, 200, 50), "Register For Push"))
+        {
+			GameObject androidM = GameObject.Find(constants.gameObjectName);
+			DontDestroyOnLoad(transform.gameObject);
+			this.gameObject.name = constants.gameObjectName;
+			RegisterForPush();
+		}
+	}
+
+	
+	public void RegisterForPush(){
+		
+    Debug.Log("Sending register request... " );
+		 object[] args1 = new object[]{constants.projectNo};
+		 object[] args0 = new object[]{constants.apiKey,constants.secretKey};
+		 object[] args2= new object[]{constants.userId};
+          object[] args3 = new object[]{constants.callBackMethod,constants.gameObjectName};
+          using (AndroidJavaClass cls_obj= new AndroidJavaClass("com.unity3d.player.UnityPlayer")) {
+          using (AndroidJavaObject act_Obj = cls_obj.GetStatic<AndroidJavaObject>("currentActivity")) {
+				act_Obj.Call("intialize",args0);
+				act_Obj.Call("setProjectNo",args1);
+				act_Obj.Call("setCurrentUser",args2);
+                act_Obj.Call("registerForNotification",args3);
+          }
+    
+      }
+	}
+		
+public void Success(string val) {
+if(val != null) {
+	Debug.Log("Push Message is Here: " + val);
+}
+}
+	
+#endif
+}
