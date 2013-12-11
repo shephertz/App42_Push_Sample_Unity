fa8937a34c2c9fb8c6ea510c438c753fa59d35e3
Vishnu Garg <vishnu.garg@shephertz.co.in>
2013-12-11 16:01:32 +0530

    Update Constants.cs

--- Assets/Constants.cs
+++ Assets/Constants.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Constants {
 
 	public const string ApiKey ="<YOUR API KEY>";
-	public const string SecretKey=="<YOUR SECRET KEY>";
+	public const string SecretKey="<YOUR SECRET KEY>";
 	public const string GoogleProjectNo="<YOUR PROJECT NO>";
 	public const string UserId="<YOUR USER ID>";
  // public string callBackMethod="<CALLBACK METHOD>";
